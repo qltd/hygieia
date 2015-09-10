28f8d4654e78fb35647eac865f3af9e40dd28a4d
Matt Restorff <inperpetuum@gmail.com>
2015-09-10 12:43:01 -0400

    Added News Feed and Single Templates

--- index.php
+++ index.php
@@ -1,43 +1,24 @@
 <?php get_header(); ?>
 
+<div id="page-content" class="body-text">
 
-    <?php if (is_category()): ?>
-    	<div id="page-headers" class="centered body-text"><h1><?php single_tag_title(); ?></h1>
-    <?php else: ?>
-    	<div id="page-content" class="center-content body-text"><?php $page = get_page(12); echo $page->post_content; ?>
-    <?php endif; ?>
-	</div>
+    <aside id="sidebar" class="Leaves">
 
-<div class="centered">
-	<div class="filter-wrap">
-			Filter by: <a href="#" class="filter-btn">Category</a>
-			<div class="ftg-filters">
-				<a href="<?php echo get_permalink(12); ?>">View All</a>
-				<?php  $categories = get_categories();
-					foreach($categories as $category){
-						echo '<a href="' . get_category_link($category->term_id) . '">' . $category->name . '</a>';
-					}
-				?>	
-			</div>
-		</div>
+    </aside>
 
-		<form role="search" method="get" id="searchform" action="<?php bloginfo('url'); ?>">
-    						<input type="text" value="Search" name="s" id="s" />
-    						<input type="submit" id="search-btn" title="Search" value="">
-       					</form>
-</div>
-
-<div id="blogroll" class="center-content body-text">
-	<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+    <section class="news-page">
+        <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 		<div class="post-wrap">
+			<div class="entry-meta"><?php the_date('l, j F Y'); ?></div><!-- .entry-meta -->
 			<h2 class="title"><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h2>
 			<div class="text"><?php the_excerpt(); ?></div>
-			<div class="featured-image"><?php echo wp_get_attachment_image(get_field('featured_image'), 'content-image'); ?></div>
 			<a href="<?php echo get_permalink(); ?>" class="read-more"></a>
-			<div class="entry-meta"><?php the_author(); ?> &nbsp;|&nbsp; <?php the_date(); ?> &nbsp;|&nbsp; <?php the_category(' '); ?></div><!-- .entry-meta -->
 		</div>
 	<?php endwhile; ?>
 	<div id="blog-pager" class="center-content"><?php posts_nav_link(' ', 'Newer', 'Older'); ?></div>
+
+    </section>
+
 </div>
 
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,62 +1,19 @@
 <?php get_header(); ?>
 
-<div id="breadcrumbs" class="centered breadcrumbs">
-    <?php if(function_exists('bcn_display')){ bcn_display(); }?>
+<div id="page-content" class="body-text">
+
+    <aside id="sidebar" class="<?php the_field('sidebar_background'); ?>">
+
+    </aside>
+
+    <section class="news-page">
+        <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+            <div class="entry-meta"><?php the_date('l, j F Y'); ?></div><!-- .entry-meta -->
+            <h1><?php the_title(); ?></h1>
+            <?php the_content(); ?>
+        <?php endwhile; ?>
+    </section>
+
 </div>
-
-<div id="blogroll" class="center-content body-text">
-    <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-        <div class="post-wrap">
-            <div class="blog-header">
-            <h1><strong><?php if (get_field('page_title')) { echo get_field('page_title'); } else { the_title(); } ?></strong></h1>
-            <?php if (get_field('page_sub_title')): ?><h2><?php echo get_field('page_sub_title'); ?></h2><?php endif; ?>
-                <div class="entry-meta"><span><?php the_author(); ?></span> <span><?php the_date(); ?></span> <span><?php the_category(' '); ?></span></div><!-- .entry-meta -->
-            </div>
-            <div class="text"><?php the_content(); ?></div>
-        </div>
-
-    <?php $tags = get_the_tags(); endwhile; ?>
-    <div id="blog-pager" class="center-content"><?php posts_nav_link(' ', 'Newer', 'Older'); ?></div>
-</div>
-
-
-<?php if (get_field('gallery')): ?>
-<div id="gallery-wrap" class="centered cf">
-    <div id="gallery">
-        <div class="ftg-items">          
-            <?php while(has_sub_field('gallery')):?>
-                <article class="tile">
-                <?php $thumb = wp_get_attachment_image_src(get_sub_field('image'), "gallery-grid-thumb"); 
-                    $image = wp_get_attachment_image_src(get_sub_field('image'), "full"); ?>
-                    <a class="tile-inner magnific-img" href="<?php echo $image[0]; ?>">
-                        <img class="item" src="<?php echo $thumb[0]; ?>" />
-                        <!-- <div class="caption">
-                        caption
-                        </div> -->
-                    </a>
-                </article>
-            <?php endwhile; ?>                 
-        </div>
-    </div>
-</div>
-<?php endif; ?>
-
-
-<div class="centered testimonial">
-    <blockquote>
-        &ldquo;<?php the_field('testimonial_text'); ?>&rdquo;
-    </blockquote>
-    <span>&mdash; <?php the_field('testimonial_author'); ?></span>
-</div>
-
-
-<div class="tags center-content body-text cf">
-    <div class="share"><?php echo do_shortcode('[ssba]'); ?></div>
-    <div class="entry-meta">Tags: <?php $c = 0; foreach($tags as $tag){ if ($c > 0) { echo ', '; $c++; } echo $tag->name; } ?></div>
-    <div class="entry-meta related"><span>Related: </span><?php echo do_shortcode('[relatedPosts]'); ?></div>
-</div>
-
-    <div id="blog-pager" class="center-content"><?php previous_post('%', 'Older', 'no'); ?>    <?php next_post('%','Newer','no'); ?></div>
-
 
 <?php get_footer(); ?>
